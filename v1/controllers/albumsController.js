8db8e2da404cbe327d90199388b647b73fd1c1be
osman-butt <butt.osman@gmail.com>
2023-09-19 23:40:39 +0200

    Added GET route /albums/:id

--- v1/controllers/albumsController.js
+++ v1/controllers/albumsController.js
@@ -21,7 +21,24 @@ function getAllAlbums(req, res) {
 }
 
 function getAlbumsById(req, res) {
-  res.json({ message: "GET /albums/:id not implemented yet" });
+  const id = req.params.id;
+  const query = /*sql*/ `
+    SELECT albums.*, artists.artistName as artistNames FROM albums
+        LEFT JOIN artists_albums on albums.albumID = artists_albums.albumID
+        LEFT JOIN artists on artists_albums.artistID = artists.artistID
+        WHERE albums.albumID=?;
+  `;
+  dbconfig.query(query, [id], (error, results, fields) => {
+    if (error) {
+      res.status(500).json({ message: "500 - Internal server error" });
+    } else {
+      if (results) {
+        res.json(formatAlbumsArtists(results));
+      } else {
+        res.status(404).json({ message: "404 - Could not find resource" });
+      }
+    }
+  });
 }
 
 export default {
