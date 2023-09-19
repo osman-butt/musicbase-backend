c072a71b8dfec4b1b73ae10b7b3d04ca469e57bf
osman-butt <butt.osman@gmail.com>
2023-09-19 14:00:14 +0200

    Added get route for /artists

--- v1/controllers/artistsController.js
+++ v1/controllers/artistsController.js
@@ -1,7 +1,18 @@
 import dbconfig from "../../database.js";
 
 function getAllArtists(req, res) {
-  res.json({ message: "GET /artist not implemented" });
+  const query = "SELECT * FROM artists;";
+  dbconfig.query(query, (error, results, fields) => {
+    if (error) {
+      res.status(500).json({ message: "500 - Internal server error" });
+    } else {
+      if (results) {
+        res.json(results);
+      } else {
+        res.status(404).json({ message: "404 - Could not find resource" });
+      }
+    }
+  });
 }
 
 function getArtistById(req, res) {
