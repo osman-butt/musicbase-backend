64c221d5596573227ee5e9cffb5dfcd7d899ac79
osman-butt <butt.osman@gmail.com>
2023-09-19 23:12:24 +0200

    Added GET route /artists/:id/albums/:id/songs

--- helpers.js
+++ helpers.js
@@ -1,8 +1,8 @@
-function formatArtistAlbums(artistsAlbums) {
+function formatArtistAlbums(artistAlbums) {
   // Create an object to store artists with albums as an array
   const artistsWithAlbums = {};
 
-  for (const artist of artistsAlbums) {
+  for (const artist of artistAlbums) {
     // If the artists is not in the object, add it
     if (!artistsWithAlbums[artist.artistID]) {
       artistsWithAlbums[artist.artistID] = {
@@ -30,4 +30,109 @@ function formatArtistAlbums(artistsAlbums) {
   return artistsArray;
 }
 
-export { formatArtistAlbums };
+function formatArtistAlbumSongs(artistAlbumSongs) {
+  const artistsWithAlbums = {};
+
+  for (const artist of artistAlbumSongs) {
+    // If the artists is not in the object, add it
+    if (!artistsWithAlbums[artist.artistID]) {
+      artistsWithAlbums[artist.artistID] = {
+        artistID: artist.artistID,
+        artistName: artist.artistName,
+        artistImage: artist.artistImage,
+        artistDescription: artist.artistDescription,
+        albums: [],
+      };
+    }
+
+    if (artist.albumID !== null) {
+      const album = artistsWithAlbums[artist.artistID].albums.find(
+        a => a.albumID === artist.albumID
+      );
+      if (!album) {
+        artistsWithAlbums[artist.artistID].albums.push({
+          albumID: artist.albumID,
+          albumName: artist.albumName,
+          albumImage: artist.albumImage,
+          albumReleaseDate: artist.albumReleaseDate,
+          songs: [],
+        });
+        if (artist.songID !== null) {
+          artistsWithAlbums[artist.artistID].albums[0].songs.push({
+            songID: artist.songID,
+            songName: artist.songName,
+          });
+        }
+      } else {
+        if (artist.songID !== null) {
+          artistsWithAlbums[artist.artistID].albums[0].songs.push({
+            songID: artist.songID,
+            songName: artist.songName,
+          });
+        }
+      }
+    }
+  }
+
+  const artistsArray = Object.values(artistsWithAlbums);
+  return artistsArray;
+}
+
+// function formatArtistAlbumSongs(artistAlbumSongsArray) {
+//   const transformedArray = artistAlbumSongsArray.reduce((acc, current) => {
+//     const existingArtist = acc.find(item => item.artistID === current.artistID);
+
+//     if (existingArtist) {
+//       const existingAlbum = existingArtist.album.find(
+//         album => album.albumID === current.albumID
+//       );
+
+//       if (existingAlbum) {
+//         existingAlbum.songs.push({
+//           songID: current.songID,
+//           songName: current.songName,
+//         });
+//       } else {
+//         existingArtist.album.push({
+//           albumID: current.albumID,
+//           albumName: current.albumName,
+//           albumImage: current.albumImage,
+//           albumReleaseDate: current.albumReleaseDate,
+//           songs: [
+//             {
+//               songID: current.songID,
+//               songName: current.songName,
+//             },
+//           ],
+//         });
+//       }
+//     } else {
+//       acc.push({
+//         artistID: current.artistID,
+//         artistName: current.artistName,
+//         artistImage: current.artistImage,
+//         artistDescription: current.artistDescription,
+//         album: [
+//           {
+//             albumID: current.albumID,
+//             albumName: current.albumName,
+//             albumImage: current.albumImage,
+//             albumReleaseDate: current.albumReleaseDate,
+//             songs: [
+//               {
+//                 songID: current.songID,
+//                 songName: current.songName,
+//               },
+//             ],
+//           },
+//         ],
+//       });
+//     }
+
+//     return acc;
+//   }, []);
+
+//   return transformedArray;
+// }
+
+export { formatArtistAlbums, formatArtistAlbumSongs };
--- v1/routes/artists.js
+++ v1/routes/artists.js
@@ -3,11 +3,19 @@ import artistsController from "../controllers/artistsController.js";
 
 const artistsRouter = express.Router();
 
+// GET
 artistsRouter.get("/", artistsController.getAllArtists);
 artistsRouter.get("/:id", artistsController.getArtistById);
 artistsRouter.get("/:id/albums", artistsController.getArtistWithAlbums);
+artistsRouter.get(
+  "/:artistId/albums/:albumId/songs",
+  artistsController.getArtistWithAlbumAndSongs
+);
+// POST
 artistsRouter.post("/", artistsController.addArtist);
+// PUT
 artistsRouter.put("/:id", artistsController.updateArtist);
+// DELETE
 artistsRouter.delete("/:id", artistsController.deleteArtist);
 
 export { artistsRouter };
