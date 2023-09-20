63a12530bf2d8a13943fb7d1ad397aa1dc430063
osman-butt <butt.osman@gmail.com>
2023-09-20 16:32:41 +0200

    Refractor to use "mysql2/promise"

--- v1/routes/albums.js
+++ v1/routes/albums.js
@@ -7,7 +7,7 @@ const albumsRouter = express.Router();
 albumsRouter.get("/", albumsController.getAllAlbums);
 albumsRouter.get("/:id", albumsController.getAlbumsById);
 albumsRouter.get("/:id/songs", albumsController.getAlbumWithSongs);
-albumsRouter.post("/", albumsController.createAlbum);
+albumsRouter.post("/", albumsController.addAlbum);
 albumsRouter.put("/:id", albumsController.updateAlbum);
 albumsRouter.delete("/:id", albumsController.deleteAlbum);
 
