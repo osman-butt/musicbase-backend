c4a368b813a4a1e1f970fb27b133a099adef1de3
osman-butt <butt.osman@gmail.com>
2023-09-20 02:01:56 +0200

    Added POST route /songs

--- v1/controllers/songsController.js
+++ v1/controllers/songsController.js
@@ -1,3 +1,4 @@
+import { query } from "express";
 import dbconfig from "../../database.js";
 
 function getAllSongs(req, res) {
@@ -18,10 +19,46 @@ function getAllSongs(req, res) {
 }
 
 function getSongsById(req, res) {
-  res.json({ message: "GET /songs/id is not implemented yet!" });
+  const id = req.params.id;
+  const query = /*sql*/ `
+        SELECT * FROM songs WHERE songID=?;
+    `;
+  dbconfig.query(query, [id], (error, results, fields) => {
+    if (error) {
+      res.status(500).json({ message: "500 - Internal server error" });
+    } else {
+      if (results) {
+        res.json(results);
+      } else {
+        res.status(404).json({ message: "404 - Could not find resource" });
+      }
+    }
+  });
+}
+
+function addSong(req, res) {
+  const newSong = req.body;
+  const values = [newSong.songName];
+  const query = /*sql*/ `
+    INSERT INTO songs (songName)
+    VALUES (?);
+    `;
+  dbconfig.query(query, values, (error, results, fields) => {
+    if (error) {
+      res.status(500).json({ message: "500 - Internal server error" });
+    } else {
+      if (results) {
+        res.json({ artistID: results.insertId });
+      } else {
+        res.status(404).json({ message: "404 - Could not find resource" });
+      }
+    }
+  });
+  //   res.json({ message: "GET /songs/id is not implemented yet!" });
 }
 
 export default {
   getAllSongs,
   getSongsById,
+  addSong,
 };
--- v1/routes/songs.js
+++ v1/routes/songs.js
@@ -6,5 +6,6 @@ const songsRouter = express.Router();
 // GET
 songsRouter.get("/", songsController.getAllSongs);
 songsRouter.get("/:id", songsController.getSongsById);
+songsRouter.post("/", songsController.addSong);
 
 export { songsRouter };
