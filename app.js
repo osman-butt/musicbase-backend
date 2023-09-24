a63ed534bd357446bab412b0af05aeee6d0534f5
osman-butt <butt.osman@gmail.com>
2023-09-24 16:35:48 +0200

    Redirections and error handling

--- app.js
+++ app.js
@@ -16,14 +16,29 @@ const app = express();
 app.use(express.json()); // parse incomming JSON
 app.use(cors());
 
+// Entry point
 app.get("/api/v1/", (req, res) => {
   res.json({ message: "Musicbase API" });
 });
 
+// Routes
 app.use("/api/v1/artists", artistsRouter);
 app.use("/api/v1/albums", albumsRouter);
 app.use("/api/v1/songs", songsRouter);
 
+// REDIRECTION TO API
+app.get("/", (req, res) => {
+  res.status(301).redirect("/api/v1/"); // Redirect to '/new-location'
+});
+
+// Captures undefined routes
+app.use((req, res, next) => {
+  res.status(404).json({
+    status: "404 - Resource not found",
+    message: "Ensure you are using /api/v1/[RESOURCE]",
+  });
+});
+
 app.listen(port, (req, res) => {
-  console.log(`The server is running on "http://localhost:${port}"`);
+  console.log(`The server is running on "http://localhost:${port}/api/v1"`);
 });
